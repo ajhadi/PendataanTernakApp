0439a5667901ff776d09bb1b5740946f73e6330f
Aj Hadi <me@ajhadi.space>
2024-10-21 09:48:48 +0700

    Add spalsh screen and config deploy

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -29,7 +29,9 @@ export default function RootLayout() {
 
     initializeDatabase();
     if (loaded) {
-      SplashScreen.hideAsync();
+      setTimeout(() => {
+        SplashScreen.hideAsync();
+      }, 3000);
     }
   }, [loaded]);
 
